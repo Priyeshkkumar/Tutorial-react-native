c77f53125fe9db81bfef00d16e88ad6e7a1795e6
Priyeshkkumar <priyeshkkumar@gmail.com>
2020-12-24 11:56:53 +0530

    Building ViewImage Screen

---
+++ app/screens/ViewImageScreen.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Image, StyleSheet, View } from "react-native";
+
+function ViewImageScreen(props) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.closeicons}></View>
+      <View style={styles.deleteicons}></View>
+      <Image
+        resizeMode="contain"
+        style={styles.image}
+        source={require("../assets/1.jpeg")}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  closeicons: {
+    width: 50,
+    height: 50,
+    backgroundColor: "#fca311",
+    position: "absolute",
+    top: 40,
+    left: 30,
+  },
+  container: {
+    backgroundColor: "black",
+    flex: 1,
+  },
+  deleteicons: {
+    width: 50,
+    height: 50,
+    backgroundColor: "#fca311",
+    position: "absolute",
+    top: 40,
+    right: 30,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
+
+export default ViewImageScreen;
