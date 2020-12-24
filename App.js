85fff27b51318deb6c2db212d9d7331122923070
Priyeshkkumar <priyeshkkumar@gmail.com>
2020-12-24 12:07:01 +0530

    Borders

--- App.js
+++ App.js
@@ -1,36 +1,24 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
   return (
-    <View
-      style={{
-        backgroundColor: "white",
-        flex: 1,
-        flexDirection: "row", // horizontal
-        justifyContent: "center", //align w.r.t to main axis
-        alignItems: "center",
-      }}
-    >
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <View
         style={{
           backgroundColor: "dodgerblue",
           width: 100,
           height: 100,
+          borderWidth: 10,
+          borderColor: "royalblue",
+          borderRadius: 15,
+          borderTopWidth: 20,
+          borderTopLeftRadius: 50,
         }}
-      />
-      <View
-        style={{
-          backgroundColor: "gold",
-          width: 100,
-          height: 100,
-          top: 20,
-          right: 20,
-          position: "absolute", //default is relative(we can change the position of component without affecting the whole layout)
-        }}
-      />
-      <View style={{ backgroundColor: "tomato", width: 100, height: 100 }} />
+      ></View>
     </View>
   );
 }
