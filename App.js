bb56c2b7eecb8abd021b243469aed31a1b62749c
Priyeshkkumar <priyeshkkumar@gmail.com>
2020-12-24 12:21:48 +0530

    Styling Text

--- App.js
+++ App.js
@@ -7,26 +7,21 @@ import WelcomeScreen from "./app/screens/WelcomeScreen";
 export default function App() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <View
+      <Text
         style={{
-          backgroundColor: "dodgerblue",
-          width: 100,
-          height: 100,
-          padding: 20,
-          paddingHorizontal: 10,
-          paddingLeft: 30,
+          fontSize: 20,
+          // Android and ios support different fonts, Therefore we need to
+          // to use platform API to check fonts according to OS
+          fontStyle: "italic",
+          fontWeight: "bold",
+          textTransform: "capitalize",
+          textAlign: "center",
+          lineHeight: 30,
         }}
       >
-        <View style={{ backgroundColor: "gold", width: 50, height: 50 }} />
-      </View>
-      <View
-        style={{
-          backgroundColor: "tomato",
-          width: 100,
-          height: 100,
-          margin: 20,
-        }}
-      />
+        React Native
+        iasdbiabdibaosdblaibadisbdabsdibasidbaidbiabsdiasbdiasbdiabsdib
+      </Text>
     </View>
   );
 }
