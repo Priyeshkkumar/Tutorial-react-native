212bb8122e6d2e61cc8ce2ea0dca4e1924bb53fd
Priyeshkkumar <priyeshkkumar@gmail.com>
2020-12-25 15:08:20 +0530

    Building better Forms wiht formik

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,51 +1,58 @@
-import React, { useState } from "react";
+import React from "react";
 import { Image, StyleSheet } from "react-native";
 import AppButton from "../components/AppButton";
-import AppText from "../components/AppText/AppText";
 import AppTextInput from "../components/AppTextInput";
+import { Formik } from "formik";
 
 import Screen from "../components/Screen";
 import colors from "../config/colors";
 
 function LoginScreen(props) {
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
-
   return (
     <Screen style={styles.container}>
       <Image style={styles.logo} source={require("../assets/logo.png")} />
+      <Formik
+        initialValues={{ email: "", password: " " }}
+        onSubmit={(values) => console.log(values)}
+      >
+        {({ handleChange, handleSubmit }) => (
+          <>
+            <AppTextInput
+              autoCaaitalize="none"
+              autoCorrect={false}
+              icon="email"
+              keyboardType="email-address"
+              onChangeText={handleChange("email")}
+              placeholder="Email"
+              textContentType="emailAddress" //only works on ios
+            />
+            <AppTextInput
+              autoCaaitalize="none"
+              autoCorrect={false}
+              icon="lock"
+              onChangeText={handleChange("password")}
+              secureTextEntry={true}
+              placeholder="Password"
+              textContentType="password" //only works on ios
+            />
+            <AppButton
+              //   setting the width of the button
+              style={{
+                width: "95%",
+                height: 60,
+                margin: 10,
+                backgroundColor: colors.secondary,
+              }}
+              title="Login"
+              onPress={handleSubmit} // onSubmit props of Formik will be called by handleSubmit
+            />
+          </>
+        )}
+      </Formik>
+
       {/* Since we have built a custom TextInput(for pretty styling), therefore 
       intellisence won't work here, therefore always read offical docs
       for props(goto TextInput on official docs of expo). */}
-      <AppTextInput
-        autoCaaitalize="none"
-        autoCorrect={false}
-        icon="email"
-        keyboardType="email-address"
-        onChangeText={(text) => setEmail(text)}
-        placeholder="Email"
-        textContentType="emailAddress" //only works on ios
-      />
-      <AppTextInput
-        autoCaaitalize="none"
-        autoCorrect={false}
-        icon="lock"
-        onChangeText={(text) => setPassword(text)}
-        secureTextEntry={true}
-        placeholder="Password"
-        textContentType="password" //only works on ios
-      />
-      <AppButton
-        //   setting the width of the button
-        style={{
-          width: "95%",
-          height: 60,
-          margin: 10,
-          backgroundColor: colors.secondary,
-        }}
-        title="Login"
-        onPress={() => console.log(email, password)}
-      />
     </Screen>
   );
 }
