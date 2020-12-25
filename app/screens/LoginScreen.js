40c9970bf604c1e8fd3279280084686288d09ce0
Priyeshkkumar <priyeshkkumar@gmail.com>
2020-12-25 23:35:32 +0530

    Building the form component

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Image, StyleSheet, Text } from "react-native";
-import { Formik } from "formik";
 import * as Yup from "yup";
 
 import Screen from "../components/Screen";
 import AppFormField from "../components/AppFormField";
 import SubmitButton from "../components/SubmitButton";
+import AppForm from "../components/AppForm";
 
 // Validation schema: An object that determines rules
 // for validating our form. We are defining it outside our function
@@ -19,35 +19,31 @@ function LoginScreen(props) {
   return (
     <Screen style={styles.container}>
       <Image style={styles.logo} source={require("../assets/logo.png")} />
-      <Formik
+      <AppForm
         initialValues={{ email: "", password: " " }}
         onSubmit={(values) => console.log(values)}
         validationSchema={validationSchema}
       >
-        {() => (
-          <>
-            <AppFormField
-              autoCaaitalize="none"
-              autoCorrect={false}
-              icon="email"
-              keyboardType="email-address"
-              name="email"
-              placeholder="Email"
-              textContentType="emailAddress" //only works on ios
-            />
-            <AppFormField
-              autoCaaitalize="none"
-              autoCorrect={false}
-              icon="lock"
-              name="password"
-              placeholder="Password"
-              secureTextEntry={true}
-              textContentType="password" //only works on ios
-            />
-            <SubmitButton title="Login" />
-          </>
-        )}
-      </Formik>
+        <AppFormField
+          autoCaaitalize="none"
+          autoCorrect={false}
+          icon="email"
+          keyboardType="email-address"
+          name="email"
+          placeholder="Email"
+          textContentType="emailAddress" //only works on ios
+        />
+        <AppFormField
+          autoCaaitalize="none"
+          autoCorrect={false}
+          icon="lock"
+          name="password"
+          placeholder="Password"
+          secureTextEntry={true}
+          textContentType="password" //only works on ios
+        />
+        <SubmitButton title="Login" />
+      </AppForm>
 
       {/* Since we have built a custom TextInput(for pretty styling), therefore 
       intellisence won't work here, therefore always read offical docs
